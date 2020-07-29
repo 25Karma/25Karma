e9043d7fb49998032a016d4603d9db05d549a4b1
Amos Yu <amosyu2000@gmail.com>
2020-07-29 00:44:56 -0400

    Updates

---
+++ precache-manifest.dedd1cf3932ff0c7fab0da4da0843f27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d587606c4e9c51f48c5b80a48ac595c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ee0f91f14d3a4711f53",
+    "url": "/static/css/main.5763607b.chunk.css"
+  },
+  {
+    "revision": "5127df92158a333b5cc1",
+    "url": "/static/js/2.0fdc013b.chunk.js"
+  },
+  {
+    "revision": "c3f4a91b08326551c3d2c23a3296dfa8",
+    "url": "/static/js/2.0fdc013b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee0f91f14d3a4711f53",
+    "url": "/static/js/main.d2a00716.chunk.js"
+  },
+  {
+    "revision": "7fe7b55524a8f4897686",
+    "url": "/static/js/runtime-main.9f45a456.js"
+  },
+  {
+    "revision": "2c90030680a2fafd21f53fd39a0862e7",
+    "url": "/static/media/minecraft.2c900306.otf"
+  }
+]);
