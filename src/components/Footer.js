983025cfc47280c88e276c2beec2d06e35a23c06
Amos Yu <amosyu2000@gmail.com>
2021-04-23 18:32:08 -0400

    Wrapped up Netlify migration

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,12 +13,10 @@ export function Footer(props) {
 			<span>
 				Powered by the&nbsp;
 				<ExternalLink href="https://api.hypixel.net/">Hypixel API</ExternalLink>. 
-				Hosted on&nbsp;
-				<ExternalLink href="https://pages.github.com/">GitHub Pages</ExternalLink>.
 			</span>
 			<span className="text-right">
 				{`Made with ❤️ by `}
-				<Link to="/player/a6df397efeab496d9f2a6d6eeab10470" className="link">Amos</Link>.
+				<Link to="/search/a6df397efeab496d9f2a6d6eeab10470" className="link">Amos</Link>.
 			</span>
 		</footer>
 		);
