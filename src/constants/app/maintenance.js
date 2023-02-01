5ed475d3aa2ccac71db11a8257562987624ed98f
Amos Yu <yua25@mcmaster.ca>
2023-01-31 19:10:32 -0500

    Down for maintenance

--- src/constants/app/maintenance.js
+++ src/constants/app/maintenance.js
@@ -1,6 +1,6 @@
 export const MAINTENANCE = {
-	enabled: false,
-	at: "2021/02/28, 3:09 PM EDT",
-	eta: "a few weeks",
-	memo: "",
+	enabled: true,
+	at: "2023/01/31, 3:09 PM EDT",
+	eta: "a month or two",
+	memo: "Friends API is broken. Working on a fix.",
 }
